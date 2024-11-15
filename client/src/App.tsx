432122ec51942d9a0ea00d67858f6c1eae325c65
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-11-15 15:17:23 +0800

    feat: update navigation cards with CardPortal component and add logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Automation from './pages/automation';
 import Management from './pages/management';
-import Tor from './pages/tor';
+import TOR from './pages/tor';
+import LandingPage from './pages';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li><Link to="/automation">Automation</Link></li>
-          <li><Link to="/management">Management</Link></li>
-          <li><Link to="/tor">TOR</Link></li>
-        </ul>
-      </nav>
-      
       <Routes>
         <Route path="/automation" element={<Automation />} />
         <Route path="/management" element={<Management />} />
-        <Route path="/tor" element={<Tor/>} />
+        <Route path="/tor" element={<TOR />} />
         {/* Optional: Default route */}
-        <Route path="/" element={<h1>Welcome to the Grading Management System</h1>} />
+        <Route path="/" element={<LandingPage/>}/>
       </Routes>
     </Router>
   );
